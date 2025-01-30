86afdbeb82a179ef350bfd49fdab04abe1c29ec8
Kushagra-Mangalam <kushagra.mangalam@lpu.in>
2025-01-30 10:00:08 +0530

    test

--- dsa/array_l.cpp
+++ dsa/array_l.cpp
@@ -92,6 +92,12 @@ int main(){
 cout<<endl;
 	int maxe=*max_element(arr,arr+5);
 	cout<<maxe;
+
+	int arr[]={1,2,3,4,5,6,7,8};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	for(int i=n-1;i>=0;i--){
+		cout<<arr[i];
+	}
 }
 
 
--- dsa/pep/stack/stack1.cpp
+++ dsa/pep/stack/stack1.cpp
@@ -2,6 +2,47 @@
 #include<stack>
 using namespace std;
 
+class Mystack{
+    public:
+    int *arr;
+    int top;
+    int size;
+    Mystack(){
+        arr=new int [size];
+         top=-1;
+    }
+    void push(int val){
+        if(top==size-1){
+            cout<<"stack overflow";
+            return ;
+        }
+        top++;
+        arr[top]=val;
+    }
+    void pop(){
+        if(top==-1){
+            cout<<"stack is empty";
+            return ;
+        }
+        top--;
+    }
+    bool Isempty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    int top(){
+        if(top==-1){
+            int flag= Isempty();
+            return flag;
+        }else{
+            return arr[top];
+        }
+    }
+};
 int main(){
-    
+
 }
