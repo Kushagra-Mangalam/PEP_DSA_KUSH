9d589fcfe8b61d50ab99b9e2ca8b5a04be23f69e
Kushagra-Mangalam <kushagra.mangalam@lpu.in>
2025-01-30 10:06:13 +0530

    test

--- dsa/pep/maps/unordered_maps.cpp
+++ dsa/pep/maps/unordered_maps.cpp
@@ -1,27 +1,6 @@
 #include<iostream>
-#include<stack>
 using namespace std;
 
-void rev(string rev){
-    int n=rev.length();
-    stack<string> st;
-    for(int i=0;i<n;i++){
-    string word="";
-        while(rev[i]!=' ' && i<n){
-            word+=rev[i];
-            i++;
-        }
-        st.push(word);
-    }
-
-    while(!st.empty()){
-        cout<<st.top()<<" ";
-        st.pop();
-    }
-}
-
-
 int main(){
-    string str="hello how are you";
-    rev(str);
+    
 }
